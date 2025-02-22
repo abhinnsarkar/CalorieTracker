8a0011483b285d05d03e718821a545822f4f66ba
Abhinn Sarkar <abhinnsarkar@gmail.com>
2025-02-22 15:32:41 -0800

    adjusted dashboard wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,8 @@ import FoodSearch from "./secret/FoodSearch";
 
 export default function Home() {
     return (
-        // <div className="min-h-screen bg-[#1A1F2C] bg-gradient-to-br from-background to-accent">
-        <div>
-            <main className="container mx-auto px-4 pt-20">
+        <div className="min-h-screen flex flex-col">
+            <main className="flex-1 container mx-auto px-4 pt-20">
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6 py-6">
                     <CalorieWeightGraph />
                     <FoodSearch />
